8e4061fd16df99170f42ea30ac175a9e9d51e6e1
sowjanya <swjnambati@gmail.com>
2019-08-27 22:31:13 +0530

    blg name changed

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -51,7 +51,7 @@
                 @endauth
             </div>
         @endif
-      <h4>John's Blog</h4>
+      <h4>Sowjanya's Blog</h4>
       <ul class="nav nav-pills nav-stacked">
         <li class="active"><a href="#section1">Home</a></li>
         <li><a href="#section2">Friends</a></li>
